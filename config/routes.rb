76f3bbcc47391fceccf8850cf79bcbbd7b6c6536
Erick Rivera <yomellamoerick.771@gmail.com>
2023-09-21 23:05:20 -0300

    ready

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :countdowns
+  get 'pages/index'
+  get 'pages/about'
+  get 'pages/sorry'
+  root 'countdowns#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
